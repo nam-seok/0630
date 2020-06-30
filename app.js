5083a028ed7787b4b5b487baa262bb49b162aa56
nsbaee <znxpsxk8@navere.com>
2020-06-30 17:19:20 +0900

    commit

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+require('dotenv').config();
+var path = require('path');
+//const { ENGINE_METHOD_ALL } = require('constants');
+require('ejs');
+var apiRouter = require('./routes/Router');
+const mongoose = require('mongoose');
+
+var pw = process.env.PASSWORD
+var url = `mongodb+srv://root:${pw}@cluster0.ehnlg.mongodb.net/MyDB?retryWrites=true&w=majority`
+mongoose.connect(url,{useNewUrlParser: true , useUnifiedTopology: true})
+
+
+app.set('views', path.resolve(__dirname +'/views'));   //셋팅
+app.set('view engine' , 'ejs');
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:true}));   //미들웨어
+app.use(bodyParser.json());
+app.use(express.static(__dirname + '/public'));
+
+app.use('/', apiRouter);
+const port = process.env.PORT || 3000
+
+
+app.listen(port, function() {   //서버실행
+    console.log(`Server is Starting at http://localhost:${port}`)
+
+
+});
